e3e5fc924adfb7f7acd5106fbb3aa5a63d53db56
kinders2089 <122867908+kinders2089@users.noreply.github.com>
2023-02-06 15:00:46 +0200

    Turpinās sekos

---
+++ skripti.js
@@ -0,0 +1,12 @@
+//alert("Čau, bambīno!")
+console.log("Ručka");
+
+var logs = document.querySelector(".chataZinas");
+var vards = document.querySelector("#vards");
+var zina = document.querySelector("#zina");
+
+function sutitZinu(){
+    console.log("Poga darbojas gan jau");
+    console.log("Ziņa ir: "+zina.value );
+
+}
