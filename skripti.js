f743ba6435ee0e2e473bc49fe64a74e529b36e12
kinders2089 <122867908+kinders2089@users.noreply.github.com>
2023-03-08 10:28:07 +0200

    Uzlabojums skripti.js

--- skripti.js
+++ skripti.js
@@ -1,19 +1,23 @@
-//alert("Čau, bambīno!")
-console.log("Ručka");
+//alert("Čau!")
+console.log("tests");
 
 var logs = document.querySelector(".chataZinas");
 var vards = document.querySelector("#vards");
 var zina = document.querySelector("#zina");
 
 function sutitZinu(){
-    console.log("Poga darbojas gan jau");
-    console.log("Ziņa ir: "+zina.value );
+    console.log("Poga darbojas");
+    console.log("ziņu ir: "+zina.value);
 
-    logs.innerHTML =logs.innerHTML +"</br>"+ zina.value;
+    logs.innerHTML = logs.innerHTML +"</br>"+ zina.value;
 }
 
 async function ieladetZinas(){
     let datiNoServera = await fetch("zinas.txt");
     let dati = await datiNoServera.text();
     console.log(dati);
+    logs.innerHTML = dati;
 }
+
+//reizi 1 sekundē izpilda funkciju ieladetZinas()
+setInterval(ieladetZinas, 1000)
